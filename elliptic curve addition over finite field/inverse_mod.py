8c9b2605a400dcf1c9c3fd98b82be03e3df66195
itu-itis21-gizaw21 <gizaw21@itu.edu.tr>
2024-11-17 23:22:16 +0300

    implemen tt eculedean gcd

--- elliptic curve addition over finite field/inverse_mod.py
+++ elliptic curve addition over finite field/inverse_mod.py
@@ -1,15 +1,26 @@
 def extended_eculidean(a,b):
-
-    r = [a,b]
-    s = [1,0]
-    t = [0,1]
-    q = 0
+    placeholder = 1
+    r = [a,b,placeholder]
+    s = [1,0,placeholder]
+    t = [0,1,placeholder]
+    print("r","s","k")
     k = 2
     while(r[k-2] != 0):
-        q = r
-
-    t0 = 0
-    t1 = 1
-    k = 2
+        k = 2
+        rc = r
+        sc = s
+        tc = t
+       
+        q = rc[k-2] // rc[k-1]
+        r[k] = rc[k-2] % rc[k-1]
+        s[k] = sc[k-2] - q * sc[k-1]
+        t[k] = tc[k-2] - q * tc[k-1]
+        k = k + 1
+        k = k % 3
+        
+        print(r)
+    
+    return r[k-2]
 
      
+extended_eculidean(12,5)
